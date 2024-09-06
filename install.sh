1be18e1525333ee314137cdbe2a4d165c7bff38c
LeoLeoni <jfleone97@gmail.com>
2024-09-06 12:45:42 -0400

    added machine name and tweaked install script

--- install.sh
+++ install.sh
@@ -1,14 +1,20 @@
 #!/bin/zsh
+
 # Install packages for ubuntu/debian
 if [ -d "/bin/apt" ]; then
   sudo apt update
-  yes | sudo apt install tmux fzf zoxide stow
+  yes | sudo apt install tmux fzf stow
   # ubuntu/debian doesn't ship with latest nvim
-  sudo wget -O /usr/bin/nvim https://github.com/neovim/neovim/releases/download/v0.10.0/nvim.appimage 
-  sudo chmod 555 /usr/bin/nvim
+  sudo wget -O /usr/bin/nvim https://github.com/neovim/neovim/releases/download/v0.10.1/nvim.appimage
+  sudo chmod 755 /usr/bin/nvim
 
   if [ -d "/bin/snap" ]; then 
     yes | sudo snap install alacritty --classic
+  fi
+
+  # Needed for zsh
+  if [ -d "/bin/zoxide" ]; then
+    curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
   fi
 fi
 
