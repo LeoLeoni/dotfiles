e7d40d3f42ef4e8dfa3f1ab3e201dd4597c36dd8
LeoLeoni <jfleone97@gmail.com>
2024-10-31 10:26:37 -0400

    Added autocmd to save cursor position on exit

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -36,3 +36,19 @@ autocmd({ "InsertLeave", "BufWritePost" }, {
     require("lint").try_lint()
   end,
 })
+
+-- Save cursor position on file open/close
+autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    local line = vim.fn.line "'\""
+    if
+      line > 1
+      and line <= vim.fn.line "$"
+      and vim.bo.filetype ~= "commit"
+      and vim.fn.index({ "xxd", "gitrebase" }, vim.bo.filetype) == -1
+    then
+      vim.cmd 'normal! g`"'
+    end
+  end,
+})
