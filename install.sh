4eef004260cf9ef7fee77bec846f5f46019eecf3
Justin Leone <jfleone97@gmail.com>
2025-03-18 12:55:16 -0400

    Removed alacritty. Suppress F1 help in nvim.

--- install.sh
+++ install.sh
@@ -4,12 +4,9 @@
 if [ -d "/bin/apt" ]; then
   sudo apt update
 
-  # alacritty deps
-  yes | sudo apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
-
   yes | sudo apt install tmux fzf stow
   # ubuntu/debian doesn't ship with latest nvim
-  sudo wget -O /usr/bin/nvim https://github.com/neovim/neovim/releases/download/v0.10.2/nvim.appimage
+  sudo wget -O /usr/bin/nvim https://github.com/neovim/neovim/releases/download/v0.10.4/nvim-linux-x86_64.appimage
   sudo chmod 755 /usr/bin/nvim
 
   # Needed for zsh
@@ -18,10 +15,7 @@ if [ -d "/bin/apt" ]; then
   fi
 fi
 
-# TODO: script for installing alacritty
-
 stow zsh
-stow alacritty
 stow nvim
 stow tmux
 stow fonts
--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -12,6 +12,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<F1>", "<ESC>")
 map("n", "n", "nzz")
 map("n", "N", "Nzz")
 map("n", "*", "*zz")
